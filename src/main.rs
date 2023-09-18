f0f9d719e29a8cdc6e1be979c63a1b5ee442f159
Danila Slesarev <blackdeer1524@yandex.ru>
2023-09-18 22:34:14 +0300

    done

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use std::io::{self, Read};
+
+use program_generation::generate;
+
 pub(crate) mod expander;
 pub mod inequalities;
 pub mod program_generation;
 
 fn main() {
-    println!("Hello, world!");
+    let mut stdin = io::stdin();
+    let mut specification = String::new();
+    if let Err(e) = stdin.read_to_string(&mut specification) {
+        panic!("{}", e)
+    }
+    let generated_verification_code = generate(&specification);
+    println!("{}", generated_verification_code);
 }
