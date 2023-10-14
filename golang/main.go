8a2ab5b9c51048b1416bd75f669a5bdf1d7226f0
Slava <Slava.lokshin@mail.ru>
2023-10-14 23:51:36 +0300

    Добавил ввод параметров из консоли

--- golang/main.go
+++ golang/main.go
@@ -18,12 +18,27 @@ const (
 
 func main() {
 	fmt.Println("start fuzzing")
+	fmt.Println("Enter parametres for generating regular expressions:")
 
-	// TODO: сканирую параметры для генерации регулярок
-	regGenerator, _ := reggen.New(10, 5, 4, 30)
+	// Например, можно использовать reggen.New(10, 5, 4, 30)
+
+	var countRegex, alphabetSize, starHeight, letterCount int
+
+	fmt.Println("Regex count:")
+	_, _ = fmt.Scanf("%d\n", &countRegex)
+	fmt.Println("Alphabet size:")
+	_, _ = fmt.Scanf("%d\n", &alphabetSize)
+	fmt.Println("Star height:")
+	_, _ = fmt.Scanf("%d\n", &starHeight)
+	fmt.Println("Letter count:")
+	_, _ = fmt.Scanf("%d\n", &letterCount)
+
+	fmt.Println("Your parametres:", countRegex, alphabetSize, starHeight, letterCount)
+
+	regGenerator, _ := reggen.New(countRegex, alphabetSize, starHeight, letterCount)
 
 	err := benchmark.Start(regGenerator, rustBinaryPath, countWords, maxDumpSize)
 	if err != nil {
-		fmt.Println("ОШИБКА", err)
+		fmt.Println("ОШИБКА:", err)
 	}
 }
