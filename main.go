92a9cbdb2951df0bd14c47d59ced3b1be8eccb3b
Danila Slesarev <blackdeer1524@yandex.ru>
2023-12-09 15:08:36 +0300

    table construction

--- main.go
+++ main.go
@@ -20,14 +20,15 @@ func extractFirstRune(s string) rune {
 
 const (
 	EPSILON = 'ε'
-	EOL     = '$'
+	EOS     = '$'
 	START   = 'S'
 )
 
-func parseInput(r *bufio.Reader) (terms map[rune]struct{}, nonTerms map[rune][]string) {
-	nonTerms = make(map[rune][]string)
+func parseInput(r *bufio.Reader) (terms map[rune]struct{}, var2productions map[rune][]string) {
+	var2productions = make(map[rune][]string)
+
 	terms = make(map[rune]struct{})
-	terms[EOL] = struct{}{}
+	terms[EOS] = struct{}{}
 
 	nonTerminalsStr, err := r.ReadString('\n')
 	if err != nil {
@@ -68,24 +69,8 @@ func parseInput(r *bufio.Reader) (terms map[rune]struct{}, nonTerms map[rune][]s
 			continue
 		}
 		varName := extractFirstRune(lhs)
-		nonTerms[varName] = make([]string, 0)
 
-		rhs := strings.Trim(splitRes[1], " ")
-		rightProductions := strings.Split(rhs, "|")
-		for i := range rightProductions {
-			rightProductions[i] = strings.Trim(rightProductions[i], " ")
-			if len(rightProductions[i]) == 0 {
-				log.Println("warning [", lineNumber, "]:", i, "th production is empty")
-				continue
-			}
-
-			if varName == START {
-				rightProductions[i] = fmt.Sprintf("%s%c", rightProductions[i], EOL)
-			}
-			nonTerms[varName] = append(nonTerms[varName], rightProductions[i])
-		}
-
-		if _, ok := nonTerms[varName]; ok {
+		if _, ok := var2productions[varName]; ok {
 			log.Println(
 				"warning [",
 				lineNumber,
@@ -94,6 +79,28 @@ func parseInput(r *bufio.Reader) (terms map[rune]struct{}, nonTerms map[rune][]s
 				" already defined, thus overwriting",
 			)
 		}
+		var2productions[varName] = make([]string, 0)
+
+		rhs := splitRes[1]
+		rightProductions := strings.Split(rhs, "|")
+		for i := range rightProductions {
+			rightProductions[i] = strings.Trim(rightProductions[i], " ")
+			if len(rightProductions[i]) == 0 {
+				log.Println(
+					"warning [",
+					lineNumber,
+					"]:",
+					i,
+					"th production is empty. To set up ε-production you have to use 'ε' explicitly",
+				)
+				continue
+			}
+
+			if varName == START {
+				rightProductions[i] = fmt.Sprintf("%s%c", rightProductions[i], EOS)
+			}
+			var2productions[varName] = append(var2productions[varName], rightProductions[i])
+		}
 
 		if err != nil {
 			if err == io.EOF {
@@ -101,22 +108,6 @@ func parseInput(r *bufio.Reader) (terms map[rune]struct{}, nonTerms map[rune][]s
 			}
 			panic(err)
 		}
-	}
-}
-
-type NonTerminalInfo struct {
-	isNullGenerating bool
-	// productions      []string
-	// nextTerminals    []rune
-	// nextVariables    []rune
-}
-
-func newNonTerminalInfo() *NonTerminalInfo {
-	return &NonTerminalInfo{
-		// productions:      make([]string, 0),
-		// nextTerminals:    make([]rune, 0),
-		// nextVariables:    make([]rune, 0),
-		isNullGenerating: false,
 	}
 }
 
@@ -131,7 +122,6 @@ func getEpsInfo(terms map[rune]struct{}, productions map[rune][]string) (res map
 		GREY               // yet to finish processing
 		BLACK              // processed
 	)
-	// TODO: (Optional) recursion check (when there are still grey nodes)
 	visited := make(map[rune]color, len(productions))
 
 	for key := range productions {
@@ -140,18 +130,12 @@ func getEpsInfo(terms map[rune]struct{}, productions map[rune][]string) (res map
 	}
 
 	var visitVar func(variable rune) bool
-
 	visitVar = func(variable rune) bool {
-		if _, ok := res[variable]; !ok {
-			panic(fmt.Sprintf("unknows variable: %c", variable))
-		}
-
 		if visited[variable] == GREY {
 			return true
 		} else if visited[variable] == BLACK {
 			return res[variable]
 		}
-
 		visited[variable] = GREY
 
 		isNullGenerating := false
@@ -164,20 +148,22 @@ func getEpsInfo(terms map[rune]struct{}, productions map[rune][]string) (res map
 			isEpsProduction := true
 			for _, c := range prod {
 				if _, ok := terms[c]; ok {
-					if c != EOL {
-						isEpsProduction = false
-					}
+					isEpsProduction = false
 					continue
 				}
 				isEpsProduction = isEpsProduction && visitVar(c)
+				if !isEpsProduction {
+					break
+				}
 			}
 
 			isNullGenerating = isNullGenerating || isEpsProduction
+			if isNullGenerating {
+				break
+			}
 		}
-
-		visited[variable] = BLACK
-
 		res[variable] = isNullGenerating
+		visited[variable] = BLACK
 		return isNullGenerating
 	}
 
@@ -191,14 +177,16 @@ func getEpsInfo(terms map[rune]struct{}, productions map[rune][]string) (res map
 	return res
 }
 
-func First(
+func getFirstInfo(
 	terms map[rune]struct{},
 	productions map[rune][]string,
 	epsInfo map[rune]bool,
 ) map[rune]map[rune]struct{} {
 	res := make(map[rune]map[rune]struct{})
 
+	// NOTE: cyclic dependencies detection is needed, but who cares?
 	visited := make(map[rune]struct{})
+
 	var varFirst func(nonterm rune)
 	varFirst = func(nonterm rune) {
 		if _, ok := visited[nonterm]; ok {
@@ -207,8 +195,7 @@ func First(
 		visited[nonterm] = struct{}{}
 
 		first := make(map[rune]struct{})
-		prods := productions[nonterm]
-		for _, prod := range prods {
+		for _, prod := range productions[nonterm] {
 			if len(prod) == 1 && extractFirstRune(prod) == EPSILON {
 				continue
 			}
@@ -274,7 +261,7 @@ func topoSort(dependencies map[rune]map[rune]struct{}) []rune {
 	var topoVisit func(rune)
 	topoVisit = func(v rune) {
 		if visited[v] == GREY {
-			panic(fmt.Sprintf("Found cyclic dependency on var %c", v))
+			panic(fmt.Sprintf("cyclic dependency found on var %c", v))
 		}
 
 		visited[v] = GREY
@@ -296,7 +283,8 @@ func topoSort(dependencies map[rune]map[rune]struct{}) []rune {
 	return res
 }
 
-func Follow(terms map[rune]struct{},
+func getFollowInfo(
+	terms map[rune]struct{},
 	productions map[rune][]string,
 	epsInfo map[rune]bool,
 	firstInfo map[rune]map[rune]struct{},
@@ -312,6 +300,10 @@ func Follow(terms map[rune]struct{},
 		for _, prod := range prods {
 			for i := 0; i < len(prod)-1; i++ {
 				f, s := extractPair(prod[i : i+2])
+				if _, ok := terms[f]; ok {
+					continue
+				}
+
 				if _, ok := terms[s]; ok {
 					followSets[f][s] = struct{}{}
 				} else {
@@ -332,6 +324,71 @@ func Follow(terms map[rune]struct{},
 	}
 
 	return followSets
+}
+
+func strInfo(
+	alpha string,
+	terms map[rune]struct{},
+	epsInfo map[rune]bool,
+	firstInfo map[rune]map[rune]struct{},
+	followInfo map[rune]map[rune]struct{},
+) (first map[rune]struct{}, eps bool) {
+	res := make(map[rune]struct{})
+	if len(alpha) == 0 {
+		return res, false
+	}
+
+	for _, c := range alpha {
+		if _, ok := terms[c]; ok {
+			res[c] = struct{}{}
+			return res, false
+		}
+		mergeInPlace(res, firstInfo[c])
+		if !epsInfo[c] {
+			return res, false
+		}
+	}
+	return res, true
+}
+
+func BuildTable(
+	terms map[rune]struct{},
+	productions map[rune][]string,
+) map[rune]map[rune]string {
+	epsInfo := getEpsInfo(terms, productions)
+	firstInfo := getFirstInfo(terms, productions, epsInfo)
+	followInfo := getFollowInfo(terms, productions, epsInfo, firstInfo)
+
+	res := make(map[rune]map[rune]string, len(productions))
+	for v := range productions {
+		res[v] = make(map[rune]string, len(terms))
+		for t := range terms {
+			res[v][t] = ""
+		}
+	}
+
+	for v, prods := range productions {
+		for _, prod := range prods {
+			// https://web.cs.wpi.edu/~kal/PLT/PLT4.3.html
+			firstSet, eps := strInfo(prod, terms, epsInfo, firstInfo, followInfo)
+			for a := range firstSet {
+				if res[v][a] != "" {
+					panic("conflict found")
+				}
+				res[v][a] = prod
+			}
+			if eps {
+				for b := range followInfo[v] {
+					if res[v][b] != "" {
+						panic("conflict found")
+					}
+					res[v][b] = prod
+				}
+			}
+		}
+	}
+
+	return res
 }
 
 func main() {
